0d5b329ee187236bd563cc4fa9231ef69c9907ac
梁鹏程 <liangpengcheng@users.noreply.github.com>
2023-04-28 11:57:23 +0800

    Update split.py
    提升处理效率

--- split.py
+++ split.py
@@ -9,6 +9,14 @@ import time
 listen_path = './src_imgs'
 out_path = './out_imgs'
 
+def is_file_writeable(path):
+    try:
+        f = open(path, 'a')
+        f.close()
+    except IOError:
+        return False
+    return True
+
 class MyHandler(FileSystemEventHandler):
     #def on_any_event(self, event):
     #    print(event.event_type, event.src_path)
@@ -16,9 +24,12 @@ class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
         #print("on_created", event.src_path)
         #print(event.src_path.strip())
-        time.sleep(3)
+        path = event.src_path.strip()
+        # 判断path 是否可写
+        while is_file_writeable(path) == False:
+            time.sleep(0.1)
+            print("wait for writeable")
         try:
-            path = event.src_path.strip()
             if ".jpg" in path or "jpeg" in path or "png" in path or "jfif" in path:
                 single_img.split_single_img(path,out_path)
         except :
